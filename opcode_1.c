63a6cb265d25bbd20539715831ac9383a071d63f
Angie <angie.dar66@gmail.com>
2022-10-10 10:50:50 +0300

    Add push,pop and it opcodes

--- opcode_1.c
+++ opcode_1.c
@@ -50,3 +50,47 @@ void push(stack_t **stack, unsigned int line_cnt)
 		}
 	}
 }
+
+/**
+ * pint - print the value at the top of the stack
+ * @stack: stack given by main in start.c
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+
+void pint(stack_t **stack, unsigned int line_cnt)
+{
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
+		exit(EXIT_FAILURE);
+
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+* pop - pops the very top element of the stack
+* @stack: stack given by main in start.c
+* @line_cnt: line number for error messages
+*
+* Return: void
+*/
+void pop(stack_t **stack, unsigned int line_cnt)
+{
+	stack_t *tmp = NULL;
+
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_cnt);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = (*stack)->next;
+	free(*stack);
+	*stack = tmp;
+	if (!*stack)
+		return; /* prevents errors cause next line might assign a NULL */
+	(*stack)->prev = NULL;
+}
